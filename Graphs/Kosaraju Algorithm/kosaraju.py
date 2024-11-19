468f5bacbb4a9da07e41d81abe749413a5e9c928
Suyash Saxena <suyashsincever13@gmail.com>
2024-11-20 03:13:14 +0530

    Adding other functions as well like reversing the graph , using dfs

--- Graphs/Kosaraju Algorithm/kosaraju.py
+++ Graphs/Kosaraju Algorithm/kosaraju.py
@@ -4,20 +4,44 @@ class Graph:
         self.graph = defaultdict(list)
         self.V=V 
     
-    def add_edge(u,v):
+    def add_edge(self,u,v):
         self.graph[u].append(v)
         self.graph[v].append(u)
     
 
-    def dfs(node,stack):
+    def dfs(self,node,visited,stack):
+        visited.add(node)
+        stack.append(node)
+        for neighbor in self.graph[node]:
+            if neighbor not in visited:
+                self.dfs(neighbor,visited,stack)
+        
+    def fill_orders(self,vertex,stack):
+        visited=set()
+        for i in range(vertex):
+            if i not in visited:
+                dfs(i,visited,stack)
+        return stack
+
+    def reverse_graph(self):
+        transformed_graph= Graph(self.V)
+        for i in range(self.V):
+            for neighbor in self.graph:
+                transformed_graph.graph[neighbor].append(i)
+        self.graph=transformed_graph
+        return self.graph
 
 
+        for i in range(self.V):
+            for neighbor 
 
 
 
     def kosaraju_scc():
         stack=deque([])
         fill_orders(0,stack)
+        #Reverse the graph 
+        reverse_graph()
         scc=[]
         while stack:
             #Making another dfs to get the items from the stack 

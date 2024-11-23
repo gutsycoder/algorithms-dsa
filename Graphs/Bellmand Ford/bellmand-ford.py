73b6328277f3ad0e1780715783faa75c5899c9e2
Suyash Saxena <suyashsincever13@gmail.com>
2024-11-24 00:56:36 +0530

    Bellman ford algorithm

--- Graphs/Bellmand Ford/bellmand-ford.py
+++ Graphs/Bellmand Ford/bellmand-ford.py
@@ -14,6 +14,11 @@ def bellmanFord(V,edges,src):
         for edge in edges:
             u,v,wt=edge 
             # Write the logic for relaxation of the edge 
+            if dist[u]+wt<dist[v]:
+                dist[v]=dist[u]+wt
+                if i==V-1:
+                    return None
+    return dist
 
 
 
@@ -27,5 +32,7 @@ if __name__=="__main__":
     edges= [[1, 3, 2], [4, 3, -1], [2, 4, 1], [1, 2, 1], [0, 1, 5]]
     src=0
     ans = bellmanFord(V,edges,src)
+    if not ans:
+        print("Negative Cycle Detected")
     print(' '.join(map(str,ans)))
 
